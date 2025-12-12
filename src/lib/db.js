6df3638e3864753074c237225a0bda2952d7e447
Vijendra Singh Panwar <46666066+VijenSingh@users.noreply.github.com>
2025-12-12 12:00:24 +0000

    update db code

--- src/lib/db.js
+++ src/lib/db.js
@@ -6,7 +6,7 @@ export const connectDB = async () => {
   if (isConnected) return;
   try {
     if (!process.env.MONGO_URI) throw new Error("MONGO_URI not defined");
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, { dbName: "trading_dashboard" });
     isConnected = true;
     console.log("MongoDB connected successfully");
   } catch (err) {
